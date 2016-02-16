7b4ceb107564cee1258840ca8a4d82347efc1ec0
firdaussamad <firdaussamad94@gmail.com>
2016-02-17 02:54:18 +0800

    First commit

---
+++ PalindromeTest/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Word:");
+            string word = Console.ReadLine();
+
+            char[] charArray = word.ToCharArray();
+            Array.Reverse(charArray);
+
+            string msg = new string(charArray);
+            string message = (string.Equals(word,msg,StringComparison.OrdinalIgnoreCase) )? "Yes . It is palindrome": "No";
+
+            Console.WriteLine(message);
+            Console.ReadLine();
+        }
+    }
+}
